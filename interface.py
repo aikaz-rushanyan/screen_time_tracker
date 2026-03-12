9024dc856a5f9a75b0145653a276506d87f3067e
sus <rushanyan8477@gmail.com>
2026-03-12 19:38:55 +0300

    Initial commit

---
+++ interface.py
@@ -0,0 +1,64 @@
+import customtkinter as ctk
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import sqlite3
+
+# Настройки внешнего вида окна
+ctk.set_appearance_mode("dark")  # Темная тема
+ctk.set_default_color_theme("blue")
+
+# Создаем главное окно
+app = ctk.CTk()
+app.title("Аналитика экранного времени")
+app.geometry("1000x600")
+
+# --- ТВОЙ КОД ИЗ ANALYST.PY ---
+conn = sqlite3.connect('data/screen_time.db')
+df = pd.read_sql('SELECT * FROM screen_time_log', conn)
+conn.close()
+
+duration_sort = df.groupby('process_name')['duration_seconds'].agg(['sum']).sort_values(by='sum') / 60
+
+ds_pie = duration_sort.copy()
+mask_pie = ds_pie['sum'] < ds_pie['sum'].median()
+ds_pie.index = ds_pie.index.to_series().mask(mask_pie, 'other')
+ds_pie = ds_pie.groupby(ds_pie.index).sum()
+
+top_three = duration_sort['sum'][-3:].to_list()
+colors = ['#ff9999' if i in top_three else '#66b3ff' for i in duration_sort['sum']]
+
+# Чтобы график красиво смотрелся в темной теме, можно задать стиль
+plt.style.use('dark_background') 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+
+# Делаем фон фигуры прозрачным, чтобы он сливался с окном CustomTkinter
+fig.patch.set_facecolor('#998877') 
+ax[0].set_facecolor('#777777')
+
+ax[0].barh(duration_sort.index.to_list(), duration_sort['sum'], color=colors, edgecolor='black')
+ax[0].set_title('Экранное время')
+ax[0].set_ylabel('Программы')
+ax[0].set_xlabel('Время в мин.')
+
+ax[1].pie(data=ds_pie, x='sum', labels=ds_pie.index.to_list(), autopct='%1.0f%%')
+
+fig.subplots_adjust(left=None,
+    bottom=None,
+    right=None,
+    top=None,
+    wspace=0.5,)
+# ------------------------------
+
+# --- МАГИЯ ИНТЕГРАЦИИ ---
+# 1. Берем твой fig и упаковываем его в специальный холст для Tkinter/CustomTkinter
+# canvas_frame = ctk.CTkFrame(app)
+# canvas_frame.pack()
+# ROW = ctk.CTkFrame(canvas_frame)
+# canvas = FigureCanvasTkAgg(fig, master=app)
+# canvas.draw()
+
+# # 2. Размещаем этот холст внутри нашего окна (с отступами)
+# f = canvas.get_tk_widget()
+# ctk.CTkButton(ROW, 140, 28, text='button', command=f.pack)
+app.mainloop()
---
+++ main.py
@@ -0,0 +1,116 @@
+import time
+import psutil
+import win32gui
+import win32process
+import sqlite3
+from datetime import datetime
+import json
+
+with open('config/process_names.json', 'r', encoding='utf-8') as file:
+    names = json.load(file)
+
+conn = sqlite3.connect('data/screen_time.db')
+cursor = conn.cursor()
+
+def run_query(query, params=(), many=False): #запрос SQL, кортеж значений, executemany вкл/выкл
+    result = None
+
+    if query.strip().upper().startswith('SELECT'):
+        cursor.execute(query, params)
+        result = cursor.fetchall()
+
+    else:
+        
+        if many:
+            cursor.executemany(query, params)
+        else:
+            cursor.execute(query, params)
+
+    return result
+
+run_query('''
+    CREATE TABLE IF NOT EXISTS screen_time_log(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        process_name TEXT,
+        process_name_usable TEXT,
+        window_title TEXT,
+        start_time DATETIME,
+        end_time DATETIME,
+        duration_seconds INTEGER  
+        )
+''')
+
+def get_current_window():
+    current_window_handle = win32gui.GetForegroundWindow()
+    current_window_title = win32gui.GetWindowText(current_window_handle)
+
+    _, pid = win32process.GetWindowThreadProcessId(current_window_handle)
+
+    if pid <= 0:
+        return ('System', current_window_title)
+    
+    try:
+        process = psutil.Process(pid)
+        process_name = process.name()
+
+        return (process_name, current_window_title)
+    
+    except (psutil.NoSuchProcess, psutil.AccessDenied, ValueError):
+        return ('Unknown.exe', current_window_title)
+
+def generate_name(proc_name):
+    name = proc_name.split('.')[0].replace('_', ' ')
+    name = ''.join([i for i in name if not i.isdigit()]) + '*'
+    
+
+    return name
+
+def save_log_entry(last_w, start_t, end_t):  
+    duration = int((end_t - start_t).total_seconds())
+
+    if duration > 0:
+        process_name, window_title = last_w
+
+        if process_name not in names:
+            names[process_name] = generate_name(process_name)
+            with open('config/process_names.json', 'w', encoding='utf-8') as f:
+                json.dump(names, f, ensure_ascii=False, indent=4)
+                    
+        run_query('''INSERT INTO screen_time_log(process_name, process_name_usable, window_title, start_time, end_time, duration_seconds) 
+                    VALUES (?, ?, ?, ?, ?, ?)''', 
+                    (process_name, 
+                    names[process_name],
+                    window_title, 
+                    start_t.strftime('%Y-%m-%d %H:%M:%S'), 
+                    end_t.strftime('%Y-%m-%d %H:%M:%S'), 
+                    duration))
+        
+        conn.commit()          
+        print(f'Сохранено: {process_name} | {duration}')
+
+try:
+    last_window = None
+    start_time = datetime.now()
+
+    while True:
+        current_window = get_current_window()
+
+        if current_window != last_window:
+            now = datetime.now()
+
+            if last_window is not None:
+                save_log_entry(last_window, start_time, now)
+                
+
+        last_window = current_window
+        start_time = now
+    
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    now = datetime.now()
+    if last_window is not None:
+        save_log_entry(last_window, start_time, now)
+    
+    conn.close()
+    print('Сохранено и становлено.')

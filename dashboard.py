925fc9e920bb5c3fc36c7cf2457700f44d055e46
sus <rushanyan8477@gmail.com>
2026-03-17 13:38:18 +0300

    Updated dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,17 @@
-#import streamlit as st
-from charts import create_charts
+from charts import create_barh, create_pie
 import pandas as pd
 import matplotlib.pyplot as plt
 import sqlite3
+import streamlit as st
 
-#st.title("📊 Аналитика моего экранного времени")
+st.title("📊 Аналитика моего экранного времени")
 
 conn = sqlite3.connect('data/screen_time.db')
 df = pd.read_sql('SELECT * FROM screen_time_log', conn)
 conn.close()
 
-fig = create_charts(df)
+fig_bar = create_barh(df, size=(8, 6), quality=300)
+fig_pie = create_pie(df, size=(8, 6), quality=100)
 
-plt.show()
-
-#st.pyplot(fig)
+st.pyplot(fig_bar)
+st.pyplot(fig_pie)
